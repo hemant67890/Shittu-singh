c1c51e65970d8c8ce639cfb47bad8cf737837641
Shittu-kumar195 <southstatus31@gmail.com>
2025-03-04 14:54:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "Ricin"
-BOT_TOKEN = "6334315676:AAGdvwMNOBNnYVtD0mpeb8ZVmiBMupXzdy4"
-API_ID = 21565925
-API_HASH = "7f45d2c2ea2d654d8cff7e7fd9eaf5c1"
+BOT_NAME = "ᗷᕼᑌᗰIᕼᗩᖇ"
+BOT_TOKEN = "8005429189:AAHquY4qiB2nNJIVmUUbhkuUWGdHE9Qev4g"
+API_ID = "20114039"
+API_HASH = "87297b8f3cc8fc9bbce591ad30da5896"
 MONGO_URI = "mongodb+srv://Ricin:x8ErVo1DSSzuggqx@ricin.dudgw.mongodb.net/?retryWrites=true&w=majority&appName=Ricin"

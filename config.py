f90f21976702c17236a58801b1f396bd5171b4a6
hemant67890 <godarasubhash337@gmail.com>
2025-03-08 23:30:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_NAME = "ᗷᕼᑌᗰIᕼᗩᖇ"
-BOT_TOKEN = "7803406440:AAHOMjK18-RvITryp32ptMiegVrGAte_qKw"
+BOT_TOKEN = ""
 API_ID = "20114039"
 API_HASH = "87297b8f3cc8fc9bbce591ad30da5896"
 MONGO_URI = "mongodb+srv://Ricin:x8ErVo1DSSzuggqx@ricin.dudgw.mongodb.net/?retryWrites=true&w=majority&appName=Ricin"
